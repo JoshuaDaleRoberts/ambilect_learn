bbc8eaeb63b20e37e0d74a43fb8ddba013ddb62d
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2022-07-18 12:33:42 -0700

    tagalog time

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ function newSentence(){
     const en1 = ['good morning', 'good day', 'good afternoon', 'good evening', 'hello', 'goodbye', 'thank you'];
     const tl1 = ['magandang umaga', 'magandang tanghali', 'magandang hapon', 'magandang gabi', 'kamusta', 'paalam', 'salamat']; 
     let a = randInt(en1.length);
+    let b = randInt(2); 
     window.enPhrase = en1[a]; 
     window.tlPhrase = tl1[a]; 
+    if (b && a<4 ){
+        window.enPhrase = en1[a] + " (polite)"; 
+        window.tlPhrase = tl1[a] + " po"
+
+    }
 }
 
 function runSentence(){
