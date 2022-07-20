10e489ead8f9d5649e54c197563e0cd8ac5ad4f0
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2022-07-19 18:10:25 -0700

    I'm doing it mr crans

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function buttonOne(){
-  const usp = new URLSearchParams
+function buttonOne(num){
+  let url = new URL( 'http://127.0.0.1:5500/index.html' )
+  const usp = new URLSearchParams()
+
+  usp.append('lesson', num); 
 }
+
+var querystring = "?lesson=1"; 
+
+document.getElementById("lesson1").addEventListener(click, buttonOne("1"));
