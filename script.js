e8915599d981a118a2bda8db1946c9130e794ddc
Joshua Roberts <joshdroberts04@gmail.com>
2022-09-04 11:46:30 -0700

    tryNumber

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ import { lessonOne, lessonTwo, lessonThree, lessonFour, lessonFive } from "./les
 var correctAudio = new Audio('correct.mp3');
 var wrongAudio = new Audio('wrong.mp3');
 
+let answerState = "correct";
+let triesLeft = 6; 
+let canTry = true;
+
 const normalize = (a) => a.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 const capitalize = (a) => a.charAt(0).toUpperCase() + a.slice(1);
 
@@ -16,6 +20,8 @@ function runSentence(){
     enPhrase.innerHTML = capitalize(window.enPhrase); 
     userAnswer.value = ""
     userAnswer.value = capitalize(window.lnPhrase); 
+    triesLeft = 6;
+    canTry = true;
 }
 
 function checkLesson(){
@@ -24,7 +30,7 @@ function checkLesson(){
     a = a.toLowerCase(); 
     b = b.toLowerCase();
     if (a == b) {
-        answer.correct(); 
+        answer.correct();
     } else if (normalize(a) == normalize(b)) {
         answer.almost(); 
     } else {
@@ -34,25 +40,64 @@ function checkLesson(){
 
 const answer = {
     wrong: function(){
-        this.clear();
-        document.getElementById("wrong").style.display = "block";
-        // wrongAudio.play(); 
+        if (canTry) {
+            answerState = "wrong"
+            this.clear();
+            document.getElementById("wrong").style.display = "block";
+            if (triesLeft > 1) {
+                triesLeft--
+                document.getElementById("wrongTryNumber").innerHTML = triesLeft
+            } else {
+                triesLeft = 0; 
+                document.getElementById("wrongTryNumber").innerHTML = triesLeft;
+                this.reveal()
+                canTry = false; 
+            }
+            // wrongAudio.play(); 
+        }
     },
     correct: function(){
-        this.clear();
-        document.getElementById("correct").style.display = "block";
-        // correctAudio.play();
+        if (canTry) {
+            answerState = "correct"
+            this.clear();
+            document.getElementById("correct").style.display = "block";
+            // correctAudio.play();
+        }
     },
     almost: function(){
-        this.clear()
-        document.getElementById("almost").style.display = "block";
-        // wrongAudio.play(); 
+        if (canTry) {
+            answerState = "almost"
+            this.clear()
+            document.getElementById("almost").style.display = "block";
+            if (triesLeft > 1) {
+                triesLeft--
+                document.getElementById("almostTryNumber").innerHTML = triesLeft
+            } else {
+                triesLeft = 0; 
+                document.getElementById("almostTryNumber").innerHTML = triesLeft;
+                this.reveal()
+                canTry = false; 
+            }
+            // wrongAudio.play(); 
+        }
     },
     clear: function(){
         document.getElementById("wrong").style.display = "none";
         document.getElementById("correct").style.display = "none";
         document.getElementById("almost").style.display = "none";
-    }
+    },
+    reveal: function(){
+        let answer = window.lnPhrase;
+        if (answerState == "wrong") {
+            document.getElementById("wrongRevealSpot").innerHTML = capitalize(answer);
+        } else if (answerState == "almost") {
+            document.getElementById("almostRevealSpot").innerHTML = capitalize(answer)
+        }
+    },
+    unreveal: function(){
+        document.getElementById("wrongRevealSpot").innerHTML = ""
+        document.getElementById("almostRevealSpot").innerHTML = ""
+    },
 }
 
 function tokenCheck(){
@@ -85,15 +130,12 @@ function doAccents(){
     answerArea.value = userInput; 
 }   
 
-function revealAnswer(){
-    
-}
 
 userAnswer.addEventListener('input', doAccents)
-document.body.onload = tokenCheck; 
-document.getElementById("giveUp").onclick = runSentence;
 
+document.getElementById("giveUp").onclick = runSentence;
 document.getElementById("done").onclick = checkLesson; 
+
 document.body.onload = tokenCheck(); 
 
 document.getElementById("wrongContinue").addEventListener("click", runSentence);
@@ -101,9 +143,7 @@ document.getElementById("correctContinue").addEventListener("click", runSentence
 document.getElementById("almostContinue").addEventListener("click", runSentence);
 document.getElementById("wrongTry").addEventListener("click", checkLesson);
 document.getElementById("almostTry").addEventListener("click", checkLesson);
-//document.getElementById("wrongReveal").addEventListener("click", );
-//document.getElementById("almostReveal").addEventListener("click", );
+document.getElementById("wrongReveal").addEventListener("click", answer.reveal);
+document.getElementById("almostReveal").addEventListener("click", answer.reveal);
 
-
-
-window.document.body.onload = answer.wrong(); 
+//window.document.body.onload = answer.wrong(); 
